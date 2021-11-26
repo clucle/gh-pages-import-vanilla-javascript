faa33e8f3100714f11f75553e345489feef4ecdd
clucle <wjdenwls123@gmail.com>
2021-11-27 00:53:00 +0900

    feat: relative path module
    Signed-off-by: clucle <wjdenwls123@gmail.com>

---
+++ module2.js
@@ -0,0 +1,5 @@
+function AlertGoodbyeWorld() {
+    alert('Goodbye, world!');
+}
+
+export { AlertGoodbyeWorld };
--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 import { AlertHelloWorld } from 'https://clucle.github.io/gh-pages-import-vanilla-javascript/module.js';
+import { AlertGoodbyeWorld } from './module2.js';
 
 AlertHelloWorld();
+AlertGoodbyeWorld();
